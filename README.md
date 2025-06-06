20ba7b6bfd2e1bfdf9d22315cbe7db2b6fbfa20f
PatrickR2001 <patrick.rrleite@gmail.com>
2025-06-06 15:11:29 +0100

    projeto Sprint5

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-5
+Esse é meu projeto do Sprint 5
---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv("C:/Users/Utilizador/OneDrive/Desktop/Projeto 5/vehicles.csv")
+
+
+
+
+build_scatter_plot = st.checkbox("Criar gráfico de dispersão")
+
+if build_scatter_plot:
+    st.write(
+        "Criando um gráfico de dispersão para comparar a relação odomometro X preço do veiculo")
+
+    fig = px.scatter(df, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+            st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            fig = px.histogram(df, x="odometer")
+            st.plotly_chart(fig, use_container_width=True)
